8dd1a532cd94e28cf2ea3fa4e7d590e774aa8918
propet <luisdomingoaranda@gmail.com>
2017-12-19 13:48:40 +0100

    shortcut scripts moved to this directory

---
+++ short_scripts/new_wall.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+import random
+
+images = os.listdir("/home/luis/Pictures/wallpapers")
+
+
+# Rename files to avoid spaces
+#for i in os.list():
+#    os.rename(i, i.replace(' ', ''))
+
+rnd_img = random.choice(images)
+
+# Try to remove the previous wallpaper if there was any
+os.system("touch ~/Pictures/wallpaper/tmp")
+os.system("rm ~/Pictures/wallpaper/*")
+
+
+# copia la imagen aleatoria a la carpeta de wallpaper como wallpaper.jpg
+os.system("cp " + "~/Pictures/wallpapers/" + rnd_img + " ~/Pictures/wallpaper/wallpaper.jpg")
